70c71ddf72771f63ea7f6f541ab094adcfb869c8
demoncommand97 <demoncommand97@gmail.com>
2020-09-17 11:12:44 +0530

    :art: homepage update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,6 +28,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 2.0,
+        title: Container(
+          width: MediaQuery.of(context).size.width/2.25,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(Icons.camera_alt, color: Colors.black, size: 30.0),
+              Text('Instagram', style: TextStyle(color: Colors.black))
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.only(right: 15.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.search, color: Colors.black, size: 30.0),
+                SizedBox(width: 15.0),
+                Icon(Icons.send, color: Colors.black, size: 30.0),
+              ],
+            ),
+          )
+        ],
+      ),
       body: _tabs.length == 0 ? Center(child: CircularProgressIndicator(backgroundColor: Colors.grey, strokeWidth: 1.0)) : _tabs[_currentTab],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentTab,
@@ -37,11 +63,11 @@ class _HomePageState extends State<HomePage> {
           });
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black)),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black)),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black)),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black)),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black)),
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
         ]
       ),
     );
