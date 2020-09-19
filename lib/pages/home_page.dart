9d1a63c5ba6e4d1482924db0dff96782db02b469
demoncommand97 <demoncommand97@gmail.com>
2020-09-19 23:29:12 +0530

    :art: updating tabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instagram_ui_flutter/tabs/home_tab.dart';
 
 class HomePage extends StatefulWidget {
@@ -32,30 +33,25 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
         elevation: 2.0,
         title: Container(
-          width: MediaQuery.of(context).size.width/2.25,
+          width: MediaQuery.of(context).size.width/2.3,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Icon(Icons.camera_alt, color: Colors.black, size: 30.0),
-              Text('Instagram', style: TextStyle(color: Colors.black))
+              Text('Instagram', style: TextStyle(color: Colors.black, fontFamily: 'Billabong', fontSize: 30.0))
             ],
           ),
         ),
         actions: <Widget>[
           Container(
             padding: EdgeInsets.only(right: 15.0),
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.search, color: Colors.black, size: 30.0),
-                SizedBox(width: 15.0),
-                Icon(Icons.send, color: Colors.black, size: 30.0),
-              ],
-            ),
+            child: Icon(Icons.send, color: Colors.black, size: 30.0),
           )
         ],
       ),
       body: _tabs.length == 0 ? Center(child: CircularProgressIndicator(backgroundColor: Colors.grey, strokeWidth: 1.0)) : _tabs[_currentTab],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         currentIndex: _currentTab,
         onTap: (int value) {
           setState(() {
@@ -63,11 +59,11 @@ class _HomePageState extends State<HomePage> {
           });
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), title: SizedBox.shrink(),),
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.grey, size: 30.0), title: SizedBox.shrink(), activeIcon: Icon(Icons.home, color: Colors.black, size: 30.0)),
+          BottomNavigationBarItem(icon: Icon(Icons.search, color: Colors.grey, size: 30.0), title: SizedBox.shrink(), activeIcon: Icon(Icons.search, color: Colors.black, size: 30.0)),
+          BottomNavigationBarItem(icon: Icon(Icons.add_circle_outline, color: Colors.grey, size: 30.0), title: SizedBox.shrink(), activeIcon: Icon(Icons.add_circle_outline, color: Colors.black, size: 30.0)),
+          BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.heart, color: Colors.grey), title: SizedBox.shrink(), activeIcon: Icon(FontAwesomeIcons.solidHeart, color: Colors.black)),
+          BottomNavigationBarItem(icon: CircleAvatar(backgroundColor: Colors.yellow, radius: 15.0), title: SizedBox.shrink())
         ]
       ),
     );
