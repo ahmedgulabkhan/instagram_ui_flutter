22aa71615eafbdea7c821c5b648a7ba2b137af22
demoncommand97 <demoncommand97@gmail.com>
2020-09-20 16:29:08 +0530

    :art: profile_tab update

--- lib/tabs/profile_tab.dart
+++ lib/tabs/profile_tab.dart
@@ -16,48 +16,75 @@ class ProfileTab extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 22.0, vertical: 35.0),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(22.0, 35.0, 22.0, 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  CircleAvatar(
-                    radius: 45.0,
-                    backgroundColor: Colors.yellow,
-                  ),
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text('21', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18.0)),
-                      SizedBox(height: 10.0),
-                      Text('Posts', style: TextStyle(fontSize: 15.0))
+                      CircleAvatar(
+                        radius: 45.0,
+                        backgroundColor: Colors.yellow,
+                      ),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text('21', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18.0)),
+                          SizedBox(height: 5.0),
+                          Text('Posts', style: TextStyle(fontSize: 15.0))
+                        ],
+                      ),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text('358', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18.0)),
+                          SizedBox(height: 5.0),
+                          Text('Followers', style: TextStyle(fontSize: 15.0))
+                        ],
+                      ),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text('293', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18.0)),
+                          SizedBox(height: 5.0),
+                          Text('Following', style: TextStyle(fontSize: 15.0))
+                        ],
+                      )
                     ],
                   ),
 
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text('358', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18.0)),
-                      SizedBox(height: 10.0),
-                      Text('Followers', style: TextStyle(fontSize: 15.0))
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                    child: Text('Mike Tyler', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold)),
                   ),
 
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text('293', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18.0)),
-                      SizedBox(height: 10.0),
-                      Text('Following', style: TextStyle(fontSize: 15.0))
-                    ],
-                  )
+                  SizedBox(height: 20.0),
+
+                  Container(
+                    height: 30.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5.0),
+                      border: Border.all(
+                        width: 0.1,
+                        color: Colors.black
+                      )
+                    ),
+                    child: Center(child: Text('Edit Profile', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.0))),
+                  ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+
+            Divider()
+          ],
         ),
       )
     );
