ba5f6663d6ceba1d11f7d389e08d353a80c1fb20
demoncommand97 <demoncommand97@gmail.com>
2020-09-27 19:19:08 +0530

    :art: messages_page update

---
+++ lib/pages/messages_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MessagesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Messages page'),
+      ),
+    );
+  }
+}
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_ui_flutter/pages/messages_page.dart';
 import 'package:instagram_ui_flutter/widgets/feed_post.dart';
 
 class HomeTab extends StatelessWidget {
@@ -12,9 +13,10 @@ class HomeTab extends StatelessWidget {
         title: Container(
           width: MediaQuery.of(context).size.width/2.3,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Icon(Icons.camera_alt, color: Colors.black, size: 30.0),
+              Icon(FontAwesomeIcons.camera, color: Colors.black, size: 30.0),
+              SizedBox(width: 30.0),
               Text('Instagram', style: TextStyle(color: Colors.black, fontFamily: 'Billabong', fontSize: 30.0))
             ],
           ),
@@ -22,7 +24,12 @@ class HomeTab extends StatelessWidget {
         actions: <Widget>[
           Container(
             padding: EdgeInsets.only(right: 20.0),
-            child: Icon(FontAwesomeIcons.paperPlane, color: Colors.black),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => MessagesPage()));
+              },
+              child: Icon(FontAwesomeIcons.paperPlane, color: Colors.black)
+            ),
           )
         ],
       ),
