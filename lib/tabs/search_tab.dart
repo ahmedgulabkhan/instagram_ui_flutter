92188ded6fef693a8fcf9bfec2c76897d3a5b632
demoncommand97 <demoncommand97@gmail.com>
2020-09-27 16:32:43 +0530

    :art: search_tab update

--- lib/tabs/search_tab.dart
+++ lib/tabs/search_tab.dart
@@ -4,8 +4,355 @@ class SearchTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('This is the Search tab'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 7.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18,
+                          ),
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, size: 30.0)
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 10.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 2.0),
+
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/3 - 2,
+                          height: MediaQuery.of(context).size.width/3 - 2,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 40.0)
+            ],
+          ),
+        ),
       ),
     );
   }
